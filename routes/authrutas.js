db8c99ebb943b6ff64efa984363435cea12656f3
Cristal230203 <clealguevara@gmail.com>
2025-12-15 19:09:46 -0500

    Subiendo arreglos

--- routes/authrutas.js
+++ routes/authrutas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/usuario');
 
 // Generar JWT Token
 const generateToken = (userId) => {
